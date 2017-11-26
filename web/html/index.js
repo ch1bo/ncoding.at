6a7d01ec5f1595e25ff60a1f2e9969cfee06958c
Sebastian Nagel <sebastian.nagel@ncoding.at>
2017-11-26 21:55:53 +0100

    Animate image loading

--- web/html/index.js
+++ web/html/index.js
@@ -1,3 +1,28 @@
+function animate(duration, fn) {
+  var start
+  function go(fn, time) {
+    fn(time-start)
+    if (time-start < duration) {
+      window.requestAnimationFrame(go.bind(undefined, fn))
+    }
+  }
+  window.requestAnimationFrame(function(time) {
+    start = time
+    go(fn, time)
+  })
+}
+
 window.addEventListener('load', function() {
-  document.querySelector('.bg').classList += ' loaded';
+  var url = 'austria-tyrol.jpg'
+  var img = new Image()
+  img.onload = function() {
+    var bg = document.querySelector('.bg')
+    bg.style.backgroundImage = 'url(' + url + ')'
+    var duration = 500
+    animate(duration, function(time) {
+      var blur = Math.max(Math.abs((duration-time)/duration*20), 0)
+      bg.style.filter = 'blur(' + blur  + 'px)'
+    })
+  }
+  img.src = url
 })
